74a1a1e875315da5d7dae7d9bdac48d520ff60aa
Ilham Zamzami <94112068+ZamzamiIlham@users.noreply.github.com>
2022-06-09 23:46:02 +0700

    Add files via upload

--- JS/main.js
+++ JS/main.js
@@ -1,1 +1,12 @@
+let user = document.querySelector('.user');
 
+document.querySelector('#user-icon').onclick = () =>{
+    user.classList.toggle('active');
+}
+
+//Navbar
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY >= 0);
+});
